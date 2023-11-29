4031f444b2877604ecc5985c592cca3938573702
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-11-29 15:38:07 +0530

    slightchangeincomponents

--- src/components/LikeImage.jsx
+++ src/components/LikeImage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HigherOrderComponent from './HigherOrderComponent'
  function LikeImage(props) {
   const {count,handleUpdate} = props
--- src/components/LikePost.jsx
+++ src/components/LikePost.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HigherOrderComponent from './HigherOrderComponent'
  function LikePost(props) {
 const {count,handleUpdate} = props
